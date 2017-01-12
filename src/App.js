91576a6486f3e44920ee7359b6d327e02ed8af1f
JonGiamp <jonathan.giamporcaro@gmail.com>
2017-01-12 15:59:32 +0100

    hello world

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router'
 import './App.css';
+import Home from './Home.js';
+import About from './About.js';
+import Inbox from './Inbox.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <ul>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/inbox">Inbox</Link></li>
+        </ul>
+
+        {/*
+          next we replace `<Child>` with `this.props.children`
+          the router will figure out the children for us
+        */}
+        {this.props.children}
       </div>
-    );
+    )
   }
 }
+
+// Finally, we render a <Router> with some <Route>s.
+// It does all the fancy routing stuff for us.
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home} />
+      <Route path="about" component={About} />
+      <Route path="inbox" component={Inbox} />
+    </Route>
+  </Router>
+), document.body)
 
 export default App;
